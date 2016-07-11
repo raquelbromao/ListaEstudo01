1e4250dbfe2f5c43824fb6fa0349b9a64dff6961
raquelbromao <raquelbromao@gmail.com>
2016-07-11 15:47:15 -0300

    Primeira lista de exercícios JAVA.

---
+++ src/Exercicio02.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Exercicio02 {
+	public static void main(String[] args) {
+		/*System.out.println("Digite o tamanho do arranjo char a ser criado:");
+		
+		Scanner output = new Scanner(System.in);
+		int tam = output.nextInt();
+		
+		char[] caracteres = new char[tam];
+		
+		for(int i=0; i<=tam-1; i++) {
+			System.out.printf("Digite o caractere da posição %i",i);			
+			char caracAtual = '0';
+			try {
+				caracAtual = (char)System.in.read();
+			} catch (IOException ioe) {
+				 ioe.printStackTrace();
+			}
+			System.out.printf("O caractere digitado foi %c", caracAtual);
+			caracteres[i] = caracAtual;
+		}
+		
+		for (char caractereAtual: caracteres) {
+			System.out.printf("Caractere: %c", caractereAtual);
+		}
+		
+		//output(close); */
+	}
+}
